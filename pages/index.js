0a18490978aae1322d8d90e74e27c7832556e718
russel-ra <46997198+russel-ra@users.noreply.github.com>
2022-04-21 12:48:33 +1000

    setup wallet functionality

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,164 @@
 import { ethers } from "ethers";
-import { useEffect } from "react";
+import Web3Modal from "web3modal";
+import { useEffect, useState } from "react";
 import Script from "next/script";
+import dynamic from "next/dynamic";
+
+const providerOptions = {};
+
+const toHex = (num) => {
+  const val = Number(num);
+  return "0x" + val.toString(16);
+}
+
+const harmonyTestnetParams = {
+    chainId: toHex(1666700000),
+    rpcUrls: ["https://api.s0.b.hmny.io"],
+    chainName: "Harmony Testnet",
+    nativeCurrency: { name: "ONE", decimals: 18, symbol: "ONE" },
+    blockExplorerUrls: ["https://explorer.pops.one/"],
+    iconUrls: ["https://harmonynews.one/wp-content/uploads/2019/11/slfdjs.png"]
+  }
+
+let web3Modal;
+if (typeof window !== 'undefined') {
+  web3Modal = new Web3Modal({
+    cacheProvider: true,
+    providerOptions
+  });
+}
 
 function HomePage() {
+  const [error, setError] = useState();
+  const [instance, setInstance] = useState();
+  const [provider, setProvider] = useState();
+  const [signer, setSigner] = useState();
+  const [account, setAccount] = useState();
+  const [network, setNetwork] = useState();
+  const [chainId, setChainId] = useState();
+
   useEffect(() => {
-      
+    if (instance?.on) {
+      const handleAccountsChanged = (accounts) => {
+        console.log("accounts changed", accounts);
+        if (accounts) setAccount(accounts[0]);
+      };
+
+      const handleDisconnect = () => {
+        console.log("disconnect", error);
+        disconnect();
+      }
+
+      const handleChainChanged = (hexChainId) => {
+        console.log("chain changed")
+        setChainId(parseInt(hexChainId, 16));
+      };
+
+      instance.on("accountsChanged", handleAccountsChanged);
+      instance.on("chainChanged", handleChainChanged);
+      instance.on("disconnect", handleDisconnect);
+
+      return () => {
+        if (instance.removeListener) {
+          instance.removeListener("accountsChanged", handleAccountsChanged);
+          instance.removeListener("chainChanged", handleChainChanged);
+          instance.removeListener("disconnect", handleDisconnect);
+        }
+      }
+    }
+  }, [instance])
+
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      connectWallet();
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log(error);
+  })
+
+  const connectWallet = async () => {
+    try {
+      const instance = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(instance);
+      const signer = provider.getSigner();
+      const accounts = await provider.listAccounts();
+      const network = await provider.getNetwork();
+      setInstance(instance);
+      setProvider(provider);
+      setSigner(signer);
+      setNetwork(network);
+      setChainId(network.chainId);
+      if (accounts) setAccount(accounts[0]);
+    } catch (error) {
+      setError(error);
+    }
   }
+
+  const clearState = () => {
+    setAccount();
+    setChainId();
+    setNetwork("");
+  }
+
+  const disconnect = async () => {
+    await web3Modal.clearCachedProvider();
+    clearState();
+  }
+
+  const handleNetwork = (e) => {
+    setChainId(Number(e.target.value));
+  }
+
+  const switchNetwork = async () => {
+    try {
+      await provider.provider.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: toHex(1666700000) }]
+      });
+    } catch (switchError) {
+      if (switchError.code === 4902) {
+        try {
+          await provider.provider.request({
+            method: "wallet_addEthereumChain",
+            params: [harmonyTestnetParams]
+          });
+        } catch (error) {
+          setError(error);
+        }
+      }
+    }
+  };
+  
   return (
-      <>
-      <Script
-        src="snarkjs.min.js"
-      />
-      <div>Welcome to Next.js!</div>
-      </>
+    <>
+    <Script
+      src="snarkjs.min.js"
+    />
+    <h1>Welcome to zk-hangman (WIP)</h1>
+    <div>
+    {!account ? ( 
+      <button onClick={connectWallet}> connect ur wallet </button> 
+    ) : (
+      <button onClick={disconnect}> disconnect </button>
+    )
+    }
+    </div>
+    <div>
+      {account ? (
+        <h2> Account {account} </h2>
+      ) : (
+        <h2> Account: account not connected </h2>
+      )}
+    </div>
+    <div>
+    <h2> chainID : {chainId} </h2>
+    </div>
+    { (chainId != 1666700000 && account) &&
+      <button onClick={switchNetwork}> Connect to harmony testnet </button> 
+    }
+    </>
   )
 }
 
